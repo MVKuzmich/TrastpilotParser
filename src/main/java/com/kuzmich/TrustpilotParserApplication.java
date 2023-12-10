9a71d70c598c454b5cd349320fd6c2e2fd39aea9
Mikhail Kuzmich <kmv85@mail.ru>
2023-12-10 15:32:03 +0300

    Main Spring Boot class

---
+++ src/main/java/com/kuzmich/TrustpilotParserApplication.java
@@ -0,0 +1,13 @@
+package com.kuzmich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustpilotParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustpilotParserApplication.class, args);
+	}
+
+}
