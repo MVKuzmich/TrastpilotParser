e6f265547d7db712e76d32ec83cad96dad8c9ad0
Mikhail Kuzmich <kmv85@mail.ru>
2023-12-11 16:17:18 +0300

    refactor code

--- src/main/java/com/kuzmich/service/ParserService.java
+++ src/main/java/com/kuzmich/service/ParserService.java
@@ -9,6 +9,7 @@ import com.kuzmich.exceptions.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.codec.ClientCodecConfigurer;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
@@ -26,18 +27,29 @@ import static org.springframework.util.MimeTypeUtils.TEXT_HTML;
 @Slf4j
 public class ParserService {
 
-    private static final String BASE_URL = "https://www.trustpilot.com/review/";
+    private static final String BASE_URL_ENV = "${webclient.base-url: }";
+    private static final String CACHE_EXPIRE_AFTER_DAYS = "${cache.expire.after.days:1}";
+    private static final String CACHE_MAX_SIZE = "${cache.max-size}";
     private static final String RATING_ATTRIBUTE = "data-rating-typography";
     private static final String REVIEWS_COUNT_CLASS = "typography_body-l__KUYFJ typography_appearance-subtle__8_H2l styles_text__W4hWi";
-    private final WebClient client;
-    private final Cache<String, ParsingResult> parsingDataCache = Caffeine.newBuilder()
-            .expireAfterAccess(Duration.ofDays(1L))
-            .maximumSize(10000L)
-            .build();
+    private static final String REGEX_DIGITS_ONLY = "[^0-9]";
+    private static final String DOMAIN_IS_NOT_FOUND = "Domain is not found";
 
-    public ParserService(WebClient.Builder webClientBuilder) {
-        this.client = webClientBuilder.baseUrl(BASE_URL)
+
+    private final WebClient client;
+    private final Cache<String, ParsingResult> parsingDataCache;
+
+    public ParserService(WebClient.Builder webClientBuilder, 
+                         @Value(BASE_URL_ENV) String webClientBaseUrl,
+                         @Value(CACHE_EXPIRE_AFTER_DAYS) String cacheExpireAfterDays,
+                         @Value(CACHE_MAX_SIZE) String cacheMaxSize) {
+
+        this.client = webClientBuilder.baseUrl(webClientBaseUrl)
                 .exchangeStrategies(ExchangeStrategies.builder().codecs(this::acceptedCodecs).build())
+                .build();
+        this.parsingDataCache = Caffeine.newBuilder()
+                .expireAfterAccess(Duration.ofDays(Long.parseLong(cacheExpireAfterDays)))
+                .maximumSize(Long.parseLong(cacheMaxSize))
                 .build();
     }
 
@@ -59,7 +71,7 @@ public class ParserService {
                                                 });
                                     } else {
                                         log.info("Domain {} is not found", domain);
-                                        throw new DomainNotFoundException(new ErrorResponse(domain, HttpStatus.NOT_FOUND, "Domain is not found"));
+                                        throw new DomainNotFoundException(new ErrorResponse(domain, HttpStatus.NOT_FOUND, DOMAIN_IS_NOT_FOUND));
                                     }
                                 })
                 );
@@ -69,7 +81,7 @@ public class ParserService {
     private ParsingResult parseHtml(String html) {
         Document document = Jsoup.parse(html);
         String rating = document.getElementsByAttribute(RATING_ATTRIBUTE).get(0).text();
-        String reviews = document.getElementsByClass(REVIEWS_COUNT_CLASS).get(0).text().replaceAll("[^0-9]", "");
+        String reviews = document.getElementsByClass(REVIEWS_COUNT_CLASS).get(0).text().replaceAll(REGEX_DIGITS_ONLY, "");
 
         return new ParsingResult(Integer.parseInt(reviews), Float.parseFloat(rating));
     }
